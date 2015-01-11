3dbe97b59be79b880fd10d9d5fb23bffa51b6bc1
ooz <oliverzscheyge@gmail.com>
2015-01-12 00:04:00 +0100

    Add convenient methods to Node class, fix BST deletion

--- avl_tree.py
+++ avl_tree.py
@@ -15,6 +15,18 @@ class Node(object):
         self.right = right
         self.parent = parent
 
+    def is_left_child(self):
+        return self.parent is not None and self.parent.left == self
+
+    def is_right_child(self):
+        return self.parent is not None and self.parent.right == self
+
+    def is_root(self):
+        return self.parent is None
+
+    def is_leaf(self):
+        return self.left is None and self.right is None
+
     def set_left(self, node):
         if self.left is not None:
             self.left.parent = None
@@ -61,6 +73,12 @@ class Tree(object):
     def __repr__(self):
         return repr(self.root)
 
+    def __unicode__(self):
+        return self.__repr__()
+
+    def __str__(self):
+        return self.__unicode__()
+
 class BinaryTree(Tree):
     def __init__(self, root=None):
         super(BinaryTree, self).__init__(root)
@@ -97,41 +115,38 @@ class BinaryTree(Tree):
                 self._insert_recursive(node.left, value)
 
     def delete(self, value):
-        if self.root.value == value:
-            self.root = self._delete_recursive(self.root, value)
-        else:
-            successor = self._delete_recursive(self.root, value)
+        self.root = self._delete_recursive(self.root, value)
 
     def _delete_recursive(self, root, value):
-        if root is not None:
-            if value < root.value:
-                root.left = self._delete_recursive(root.left, value)
-            elif value > root.value:
-                root.right = self._delete_recursive(root.right, value)
-            else:
-                if root.left is None:
-                    return root.right
-                elif root.right is None:
-                    return root.left
-                else:
-                    successor = self._search_successor(root)
-                    successor.set_left(root.left)
-                    successor.set_right(root.right)
-                    root.parent = None
-                    return successor
+        if root is None:
+            return None
+
+        if value < root.value:
+            root.left = self._delete_recursive(root.left, value)
+        elif value > root.value:
+            root.right = self._delete_recursive(root.right, value)
+        else:
+            if root.left is None:
+                root.parent = None
+                return root.right
+            elif root.right is None:
+                root.parent = None
+                return root.left
+
+            successor = self._search_successor(root)
+            root.value = successor.value
+            root.right = self._delete_recursive(root.right, successor.value)
+
+        return root
 
     def _search_successor(self, node):
-        if node.right.left is None:
-            successor = node.right
-            node.set_right(node.right.right)
-            return successor
-        else:
-            node = node.right
-            while node.left.left is not None:
-                node = node.left
-            successor = node.left
-            node.set_left(node.left.right)
-            return successor
+        return self._find_left_most(node.right)
+
+    def _find_left_most(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
 
 
 class AVLTree(BinaryTree):
@@ -145,18 +160,22 @@ if __name__ == '__main__':
     values = [23, 54, 72, 76, 50, 67, 17, 19, 12, 14, 9]
     for value in values:
         avl_tree.insert(value)
-        print repr(avl_tree)
 
-    avl_tree.delete(50)
+    print "Testing deletion..."
+    print repr(avl_tree)
+    avl_tree.delete(54)
+    print repr(avl_tree)
+    avl_tree.delete(23)
     print repr(avl_tree)
 
     avl_tree = AVLTree()
     values = range(13)
     for value in values:
         avl_tree.insert(value)
-        print repr(avl_tree)
 
+    print "Testing deletion..."
+    print avl_tree
     avl_tree.delete(0)
-    print repr(avl_tree)
+    print avl_tree
     avl_tree.delete(11)
-    print repr(avl_tree)
+    print avl_tree
