38b0c5d5c6148145bc8ce7e636ca76df7cc928c2
ooz <ooz@users.noreply.github.com>
2019-05-12 21:14:24 +0200

    Java jsonpath exercise baseline

---
+++ dependencies/java-jsonpath/src/test/java/java/jsonpath/JavaJsonpathCommandTest.java
@@ -0,0 +1,28 @@
+package java.jsonpath;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.context.env.Environment;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JavaJsonpathCommandTest {
+
+    @Test
+    public void testWithCommandLineOption() throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+
+        try (ApplicationContext ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)) {
+            String[] args = new String[] { "-v" };
+            PicocliRunner.run(JavaJsonpathCommand.class, ctx, args);
+
+            // java-jsonpath
+            assertTrue(baos.toString().contains("Hi!"));
+        }
+    }
+}
